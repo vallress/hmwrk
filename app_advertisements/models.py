be1e3eb6a948d068f58364e7aec4b64a28a3257f
dar <123@mail.ru>
2023-08-11 11:45:27 +0300

    lesson7

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Advertisements(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг',help_text="Отметьте, если торг уместен" )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+    
 # Create your models here.
